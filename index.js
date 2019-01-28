1cae98d06b05788bfd53d10c58dca42b4c978449
tiffanynuugen <tiffanynuugen@gmail.com>
2019-01-28 17:54:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function justInvoke(returnsThisAndArgs) {
+  return returnsThisAndArgs(this);
+}
+
+function setThisWithCall(returnsThisAndArgs, thisValue, arg) {
+  return returnsThisAndArgs.call(thisValue, arg);
+}
+
+function setThisWithApply(returnsThisAndArgs, thisValue, args) {
+  return returnsThisAndArgs.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue);
+}
+
+
+
+
+
+
+
 //Your code here
