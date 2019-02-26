ffb61807daf0dd8e2435a3587dbed298229ba480
Cheng Vue <csalvue@gmail.com>
2019-02-26 14:04:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, args) {
+  return fn.call(thisValue, args);
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(fn, thisValue) {
+  const newFn = fn.bind(thisValue);
+  return newFn;
+}
