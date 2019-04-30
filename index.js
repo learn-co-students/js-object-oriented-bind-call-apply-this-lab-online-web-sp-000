8f278d4d55fbce5c502b8d58bea6e05fca8f6243
Joe Ganobsik <jganobsik@gmail.com>
2019-04-30 20:56:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Your code here
+function justInvoke(fn) {
+
+  return fn()
+}
+
+function setThisWithCall(fn, thisVal, arg){
+    return fn.call(thisVal, arg);
+}
+
+function setThisWithApply(fn, thisVal, arg) {
+    return fn.apply(thisVal, arg);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisVal) {
+  newfn = functionToBeCopied.bind(thisVal);
+    return newfn;
+}
