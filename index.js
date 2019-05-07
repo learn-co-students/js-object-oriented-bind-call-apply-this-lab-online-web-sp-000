a8c2b968a3827b754ca7383435d4d3985a806fa6
Jcg408 <jcg321@comcas.net>
2019-05-06 21:24:22 -0400

    Done.

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ function justInvoke (fn) {
 function setThisWithCall(fn, thisValue, arg) {
     return fn.call(thisValue, arg);
 }
-
   
 function setThisWithApply(fn, thisValue, arg) {
     return fn.apply(thisValue, arg);
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue){
-   return functionToBeCopied.bind(thisValue);
-  }
+   return functionToBeCopied.bind(thisValue);  
+}
   
