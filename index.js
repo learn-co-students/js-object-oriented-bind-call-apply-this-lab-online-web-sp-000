adcecf950495b57aed91cd45942b43b8bc5a672b
Rafael Batista <RafaelB24@yahoo.com>
2019-06-01 16:36:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//Your code here
+function justInvoke(cb){
+  return cb();
+}
+
+function setThisWithCall(cb, arg1, arg2){
+  return cb.call(arg1, arg2);
+}
+
+function setThisWithApply(cb, obj, args){
+  return cb.apply(obj, args);
+}
+
+function returnNewFunctionOf(cb, arg){
+  const newFunction = cb.bind(arg);
+  return newFunction;
+}
