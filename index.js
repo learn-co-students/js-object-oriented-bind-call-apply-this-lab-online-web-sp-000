186fa597e7c15c22296fa0af472031e53c8c04ef
Daviicode <d.ydzdavid@yahoo.com>
2019-07-02 13:05:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //Your code here
+function justInvoke(fn) {
+  // debugger
+  return fn()
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg)
+  // involks the function passed to it but uses call
+  //use call method to return the fucntions this value
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args)
+  //involk function passes throught it change the this vlaue of that function passed
+  // using apply
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const copy = functionToBeCopied.bind(thisValue)
+  return copy
+
+  // return a copy of the fucntion passed throught
+  // set thisValue of the function copy
+}
